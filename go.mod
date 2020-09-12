23b8a070515ccb90caa412f7c97b70d02c7d147c
debo <dray92@uw.edu>
2020-09-12 01:15:47 -0700

    primitives for a CA

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/dray92/go-utilities
 go 1.15
 
 require (
+	github.com/google/uuid v1.1.2
+	github.com/jinzhu/copier v0.0.0-20190924061706-b57f9002281a
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/afero v1.3.5
 	github.com/stretchr/testify v1.4.0
--- security/x509.go
+++ security/x509.go
@@ -42,50 +42,53 @@ func ParsePrivateKey(pemEncodedRSAPvtKey []byte) (*rsa.PrivateKey, error) {
 func GenerateIntermediateCert(
 	parent, settingsTemplate *x509.Certificate,
 	parentPrivKey *rsa.PrivateKey,
-) (*x509.Certificate, []byte, error) {
+) (*x509.Certificate, []byte, []byte, error) {
 	templateCopy := x509.Certificate{} // TODO check w/o init
 	err := copier.Copy(&templateCopy, &settingsTemplate)
 	if err != nil {
-		return nil, nil, errors.New("failed to make a copy of the settings template")
+		return nil, nil, nil, errors.New("failed to make a copy of the settings template")
 	}
 
 	// since this is an intermediate cert, be explicit
 	templateCopy.IsCA = true
 	templateCopy.MaxPathLen = parent.MaxPathLen - 1
-
 	// -1 is treated as unset in the std lib, but we'd expect leaf certs generated
 	// from that to fail, therefore not returning an error here. TODO (this needs to be verified)
+	if templateCopy.MaxPathLen < 0 {
+		templateCopy.MaxPathLen = 0
+	}
 
-	templateCopy.MaxPathLenZero = templateCopy.MaxPathLen < 1
+	templateCopy.MaxPathLenZero = templateCopy.MaxPathLen == 0
 	templateCopy.BasicConstraintsValid = true
 
-	// TODO (debo): revisit this to see if any other template params need to be changed
 	return generateX509Cert(parent, &templateCopy, parentPrivKey)
 }
 
 // VerifyIntermediateCert verifies an intermediate certificate. Not sure if it is currently walking up the chain correctly.
-func VerifyIntermediateCert(root, intermediate *x509.Certificate) (bool, error) {
+func VerifyIntermediateCert(root, intermediate *x509.Certificate) (bool, [][]*x509.Certificate, error) {
 	roots := x509.NewCertPool()
 	roots.AddCert(root)
 	opts := x509.VerifyOptions{
 		Roots: roots,
 	}
 
-	if _, err := intermediate.Verify(opts); err != nil {
-		return false, errors.Wrap(err, "failed to verify intermediate cert")
+	var chain [][]*x509.Certificate
+	var err error
+	if chain, err = intermediate.Verify(opts); err != nil {
+		return false, nil, errors.Wrap(err, "failed to verify intermediate cert")
 	}
-	return true, nil
+	return true, chain, nil
 }
 
 // GenerateLeafCert generates a leaf cert from the given parent.
 func GenerateLeafCert(
 	parent, settingsTemplate *x509.Certificate,
 	parentPrivKey *rsa.PrivateKey,
-) (*x509.Certificate, []byte, error) {
+) (*x509.Certificate, []byte, []byte, error) {
 	templateCopy := x509.Certificate{} // TODO check w/o init
 	err := copier.Copy(&templateCopy, &settingsTemplate)
 	if err != nil {
-		return nil, nil, errors.New("failed to make a copy of the settings template")
+		return nil, nil, nil, errors.New("failed to make a copy of the settings template")
 	}
 
 	// since this is a leaf cert, be explicit
@@ -98,41 +101,54 @@ func GenerateLeafCert(
 }
 
 // VerifyLeafCert verifies a leaf certificate. Not sure if it is currently walking up the chain correctly.
-func VerifyLeafCert(root, intermediate, child *x509.Certificate) (bool, error) {
+func VerifyLeafCert(root, child *x509.Certificate, intermediates ...*x509.Certificate) (bool, [][]*x509.Certificate, error) {
 	roots := x509.NewCertPool()
-	intermediates := x509.NewCertPool()
+	intermediatePool := x509.NewCertPool()
 	roots.AddCert(root)
-	intermediates.AddCert(intermediate)
+	for _, intermediate := range intermediates {
+		intermediatePool.AddCert(intermediate)
+	}
 	opts := x509.VerifyOptions{
 		Roots:         roots,
-		Intermediates: intermediates,
+		Intermediates: intermediatePool,
 	}
 
-	if _, err := child.Verify(opts); err != nil {
-		return false, errors.Wrap(err, "failed to verify leaf cert")
+	var chain [][]*x509.Certificate
+	var err error
+	if _, err = child.Verify(opts); err != nil {
+		return false, nil, errors.Wrap(err, "failed to verify leaf cert")
 	}
-	return true, nil
+	return true, chain, nil
 }
 
 func generateX509Cert(
 	parent, template *x509.Certificate,
 	parentPrivKey *rsa.PrivateKey,
-) (*x509.Certificate, []byte, error) {
+) (*x509.Certificate, []byte, []byte, error) {
 	priv, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
-		return nil, nil, errors.New("failed to generate private key")
+		return nil, nil, nil, errors.New("failed to generate private key")
 	}
 
 	certBytes, err := x509.CreateCertificate(rand.Reader, template, parent, &priv.PublicKey, parentPrivKey)
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to create certificate")
+		return nil, nil, nil, errors.Wrap(err, "failed to create certificate")
 	}
 	cert, err := x509.ParseCertificate(certBytes)
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to parse certificate")
+		return nil, nil, nil, errors.Wrap(err, "failed to parse certificate")
 	}
 
 	b := pem.Block{Type: "CERTIFICATE", Bytes: certBytes}
 	pemEncodedCert := pem.EncodeToMemory(&b)
-	return cert, pemEncodedCert, nil
+
+	privBytes := x509.MarshalPKCS1PrivateKey(priv)
+	pemEncodedPrivKey := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: privBytes,
+		},
+	)
+
+	return cert, pemEncodedCert, pemEncodedPrivKey, nil
 }
